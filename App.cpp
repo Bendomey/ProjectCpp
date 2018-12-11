4d3805f1663a71bee8141aec4d9165402a3e8411
unknown <ebaidoo79@gmail.com>
2018-12-11 17:07:02 +0000

    lol

--- App.cpp
+++ App.cpp
@@ -1,7 +1,14 @@
 #include "School.h"
-Akankobateng1
 
-int main(int argc, char const *argv[]){
+
+int main(int argc, char *argv[]){
+	struct User user;
+	user.first_name = "Emmanuel";
+	user.last_name = "Emmanuel";
+	user.level = 2000;
+	user.id = "10659556";
+	user.pin = "1234";
+	user.department = "ComputerScience";
 	Database db;
 	db.fetchUser(1);
 	return 0;
